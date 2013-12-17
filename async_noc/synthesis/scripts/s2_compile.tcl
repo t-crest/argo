906b48c288677ff3a59554dad7cd1154deae7f80
Christoph Müller <chmy@dtu.dk>
2013-12-17 17:20:04 +0100

    script to fix sdf for simulation with modelsim added

--- async_noc/synthesis/scripts/s2_compile.tcl
+++ async_noc/synthesis/scripts/s2_compile.tcl
@@ -5,3 +5,4 @@ write -hierarchy -format verilog -output netlists/noc_netlist.v
 write_sdf netlists/noc_netlist.sdf
 write_sdc netlists/noc_netlist.sdc
 write -hierarchy -format ddc -output mapped_constrained.ddc
+exec perl scripts/fix_pipe_sdf.pl netlists/noc_netlist

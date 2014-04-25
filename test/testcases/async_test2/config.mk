8716098886723b5276ed276803b4efbd2d215bfd
Christoph Müller <chmy@dtu.dk>
2014-04-25 13:48:40 +0200

    automated test (wip, only works for single target yet)

--- test/testcases/async_test2/config.mk
+++ test/testcases/async_test2/config.mk
@@ -11,6 +11,15 @@ async_test2_description:
 	@echo "async_test2"
 	@echo "     <testcase description here>"
 
+async_test2_auto: clean async_test2_setup noc_async traffic_generators
+	$(PREFIX) vcom $(OPTIONS) $(TESTDIR)/bench/noc_tb.vhd
+	$(PREFIX) vsim -c -t 10ps -voptargs=+acc -do $(TESTDIR)/testcases/async_test2/autosim.do noc_tb | grep NI |sort |grep read > output.filtered
+	diff output.filtered ../test/testcases/async_test2/output.golden
+	@echo async_test2 passed
+
 # append this testcase to the list of active testcases
 TESTCASE_LIST += async_test2
+# add this testcase to the list of descriptions to show
 TESTCASE_DESCRIPTIONS += async_test2_description
+# this testcase can be tested & verified automatically
+AUTO_TEST += async_test2_auto
--- test/testcases/sync_test2/config.mk
+++ test/testcases/sync_test2/config.mk
@@ -8,11 +8,20 @@ sync_test2: clean sync_test2_setup noc_sync traffic_generators
 sync_test2_setup: work
 	$(PREFIX) vcom $(OPTIONS) $(TESTDIR)/testcases/sync_test2/config.vhd 
 
-sync_test2_description:
+sync_test2_description: 
 	@echo "sync_test2"
 	@echo "     <testcase description here>"
+
+sync_test2_auto: clean sync_test2_setup noc_sync traffic_generators
+	$(PREFIX) vcom $(OPTIONS) $(TESTDIR)/util/cmd_util.vhd
+	$(PREFIX) vcom $(OPTIONS) $(TESTDIR)/bench/noc_tb.vhd
+	$(PREFIX) vsim -c -t 10ps -voptargs=+acc -gSLT_WIDTH=3 -do $(TESTDIR)/testcases/sync_test2/autosim.do noc_tb | grep NI |sort |grep read > output.filtered
+	diff output.filtered ../test/testcases/sync_test2/output.golden
+	@echo sync_test2 passed
 
 # append this testcase to the list of active testcases
 TESTCASE_LIST += sync_test2
 # add this testcase to the list of descriptions to show
 TESTCASE_DESCRIPTIONS += sync_test2_description
+# this testcase can be tested automatically
+AUTO_TEST += sync_test2_auto

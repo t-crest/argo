09e84ecab6f2e64706e73adea760bf60f4b1f593
Ioannis Kotleas <ikotleas@gmail.com>
2014-05-20 16:39:09 +0200

    Set noc node to have the same interface as the synchronous one, removed synopsys library from fifo, relocated hpu ack delay to hpu_comb

--- src/routers/asynchronous/fifo.vhd
+++ src/routers/asynchronous/fifo.vhd
@@ -1,6 +1,7 @@
-library IEEE, synopsys;
+--library IEEE, synopsys;
+library IEEE;
 use IEEE.STD_LOGIC_1164.all;
-use synopsys.attributes.all;
+--use synopsys.attributes.all;
 use work.config_types.all;
 use work.noc_defs.all;
 

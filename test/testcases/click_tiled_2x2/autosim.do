e237a9dd28f9ccd95a8b5f2894761b4cdcda3bc3
Christoph Mueller <chmy@dtu.dk>
2014-07-29 20:12:27 +0200

    Testcase for the click element based implementation

---
+++ test/testcases/click_tiled_2x2/autosim.do
@@ -0,0 +1,6 @@
+set NumericStdNoWarnings 1
+view *
+#view wave
+log -r * 
+run 50 us
+exit
--- test/testcases/click_tiled_2x2/config.mk
+++ test/testcases/click_tiled_2x2/config.mk
@@ -8,6 +8,13 @@ click_tiled_2x2: clean click_tiled_2x2_setup async_rtl_asic noc_tiled_click
 click_tiled_2x2_setup: work
 	$(PREFIX) vcom $(OPTIONS) $(TESTDIR)/testcases/click_tiled_2x2/config.vhd 
 
+click_tiled_2x2_auto: clean click_tiled_2x2_setup async_rtl_asic noc_tiled_click
+	$(PREFIX) vcom $(OPTIONS) $(TESTDIR)/bench/tiled_noc_tb.vhd
+	$(PREFIX) vcom $(OPTIONS) $(SRCDIR)/noc/tiled/fake_entities/processor_traffic_generator.vhd
+	$(PREFIX) vsim -c -t 10ps -voptargs=+acc -do $(TESTDIR)/testcases/click_tiled_2x2/autosim.do tiled_noc_tb | grep NI |sort |grep read > output.filtered
+	diff -w output.filtered ../test/testcases/click_tiled_2x2/output.golden
+	@echo 2x2 click elements passed
+
 click_tiled_2x2_description:
 	@echo "click_tiled_2x2"
 	@echo "     testcase for a tiled 2x2 noc (with click elements, rather than mousetrap)"
